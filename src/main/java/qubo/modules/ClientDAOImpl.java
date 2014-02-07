3bf746746328c2abb4707f78a9ffb663ddbac6d8
Qubo Song <sqb_007@hotmail.com>
2014-02-07 23:27:38 +0800

    update

--- src/main/java/qubo/modules/ClientDAOImpl.java
+++ src/main/java/qubo/modules/ClientDAOImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Repository;
 /**
  * Created by Qubo_Song on 2/7/14.
  */
-//@Repository
+@Repository
 public class ClientDAOImpl extends SqlSessionDaoSupport implements ClientDAO {
 
     ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-context.xml");
